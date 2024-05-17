11802f2f00b3b65e6fa97e3230459ab0dc2f218c
Guilhem <guilhem.mangenot@gmail.com>
2024-05-17 22:22:22 +0200

    feat: back api

--- youlearn-app/src/api.js
+++ youlearn-app/src/api.js
@@ -1,20 +1,47 @@
 
-export async function invokePost(method, data, successMsg, failureMsg) {
-    console.log("JINVOQUE");
-    const requestOptions = {
-         method: "POST",
-         headers: { "Content-Type": "application/json; charset=utf-8" },
-         body: JSON.stringify(data)
-     };
-     const res = await fetch("/api/YouLearn/rest/"+method,requestOptions);
-     if (res.ok) console.log(successMsg);
-     else console.log(failureMsg);
- }
- 
- export async function invokeGet(method, failureMsg) {
- 
-   const res = await fetch("/api/YouLearn/rest/"+method);
-   if (res.ok) return await res.json();    
-   console.log(failureMsg);
-   return null;
- }   
+export async function invokePost(method, data) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: JSON.stringify(data)
+  };
+  const url = `/api/YouLearn/rest/${method}`;
+  const res = await fetch(url, requestOptions);
+
+  if (res.ok) 
+    console.log("POST success : " + url);
+  else 
+    console.log("POST failure : " + url);
+}
+
+export async function invokePostAndAwaitResponse(method, data) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: JSON.stringify(data)
+  };
+  const url = `/api/YouLearn/rest/${method}`;
+  const res = await fetch(url, requestOptions);
+
+  if (res.ok) {
+    console.log("POST(with response) success : " + url);
+    return await res;
+  } else {
+    console.log("POST(with response) failure : " + url);
+    return null;
+  } 
+}
+
+export async function invokeGet(method, params = {}) {
+  const queryString = new URLSearchParams(params).toString();
+  const url = `/api/YouLearn/rest/${method}` + (queryString ? `?${queryString}` : '');
+  const res = await fetch(url);
+
+  if (res.ok) {
+    console.log("GET success : " + url);
+    return await res;
+  } else {
+    console.log("GET failure : " + url);
+    return null;
+  } 
+}
