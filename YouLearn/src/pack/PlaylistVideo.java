07a6728c517df5326f579731ab7c885db11266a1
Lighar <45882804+Lighar@users.noreply.github.com>
2024-05-28 16:15:19 +0200

    add video to playlist

---
+++ YouLearn/src/pack/PlaylistVideo.java
@@ -0,0 +1,23 @@
+package pack;
+
+import pack.entities.Playlist;
+import pack.entities.Video;
+
+
+public class PlaylistVideo {
+    private Playlist playlist;
+    private Video video;
+
+    public PlaylistVideo(Playlist playlist, Video video) {
+        this.playlist = playlist;
+        this.video = video;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+}
